90eb497598d16c845e32d37db864f9ea4b0e1776
Prama003 <151642281+Prama003@users.noreply.github.com>
2024-12-21 13:43:05 +0000

    abstractions

---
+++ OOPs/abstraction.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+class Employee{
+    public:
+    string name;
+    string company;
+    int age;
+    void askforpromotion()
+    {
+        if (age>=30)
+        {
+            cout<<name<<"is"<<"promoted"<<endl;
+        }
+        else
+        {
+        cout<<name<<"is"<<" not promoted"<<endl;
+        }
+    }
+    Employee(string aname, string acompany, int aage)
+    {
+        name=aname;
+        company=acompany;
+        age=aage;
+    }
+};
+int main()
+{
+    Employee employee1("tom","youtube",25);
+    Employee employee2("sam","google",35);
+    employee1.askforpromotion();
+    employee2.askforpromotion();
+    return 0;
+}
